ccb7e36c4adb1ba5c7cd15e91844de3e250b3511
thakursammershetty <119006657+thakursammershetty@users.noreply.github.com>
2026-02-03 18:41:04 +0530

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-// REMOVED: import { CustomCursor } from "@/components/ui/custom-cursor";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -15,7 +15,7 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "Spotmies | Next Gen Solutions",
+  title: "Spotmies",
   description: "Innovative solutions to stay ahead of the competition.",
 };
 
@@ -29,7 +29,46 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${outfit.variable} antialiased no-scrollbar font-sans`}
       >
-        {/* REMOVED: CustomCursor component */}
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1674970513183525');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1674970513183525&ev=PageView&noscript=1"
+            alt="facebook-pixel"
+          />
+        </noscript>
+
+        {/* Google Analytics Script */}
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-NQF1FJCBN7`}
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NQF1FJCBN7', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
 
         {children}
       </body>
